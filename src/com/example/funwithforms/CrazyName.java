c6bf3f1cf4bdb5f715c92642b081e1efc3eb6af5
Dylan Barth <dylan.j.barth@gmail.com>
2013-02-04 23:34:09 -0800

    It just worked. Literally just worked. OMFG so awesome.

--- src/com/example/funwithforms/CrazyName.java
+++ src/com/example/funwithforms/CrazyName.java
@@ -1,6 +1,7 @@
 package com.example.funwithforms;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -9,41 +10,41 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class CrazyName extends Activity {
 	final Context context = this;
-	final Handler mHandler = new Handler();
+	TextView streamingText;
+	Handler handler = new Handler() {
+		@Override 
+		public void handleMessage(Message msg){
+			streamingText.append((CharSequence) msg.obj);
+		}
+	};
+	AtomicBoolean isRunning=new AtomicBoolean(false);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		System.out.println("onCreate()");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crazy_name);
-
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-				context);
-		// set title
+		streamingText = (TextView) findViewById(R.id.crazy_name);
+		
+		// alert dialog
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 		alertDialogBuilder.setTitle("Warning:");
-
-		// set dialog message
 		alertDialogBuilder
 				.setMessage("All your base are belong to us.")
 				.setCancelable(false)
 				.setPositiveButton("I know :(",
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog, int id) {
-								// call streamText
-								System.out.println("just before streamText.start");
-								streamText();
+								// start the handler here! 
+								startHandler();
 							}
 						});
-
-		// create alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
-
-		// show it
 		alertDialog.show();
 	}
 
@@ -65,87 +66,49 @@ public class CrazyName extends Activity {
 		Integer length = crazyString.length();
 		
 		Random rand = new Random();
-		int i = 0;
-		int j = 0;
-		while (true) {
+		for(int i=0; i < 500; i++){
 			Integer nextChar = rand.nextInt(length);
-			String c = split[nextChar];
-			textView.append(split[nextChar]);
-			if (i >= 99) {
-				i = 0;
-				j++;
-				if (j >= 50) {
-					break;
-				}
-			}
-			i++;
+			textView.append(split[nextChar]); // except send this in a message to be appended
 		}
 	}
 
-	Thread streamText = new Thread() {
-		public void run() {
-			System.out.println("inside thread streamText()");
-
-			System.out.println("1");
-			
-			// Get the crazyString from the intent, turn it into an array
-			Intent intent = getIntent();
-			
-			System.out.println("2");
-			
-			String crazyString = intent.getStringExtra(MainActivity.CRAZY_STRING);
-			
-			System.out.println("3");
-			
-			String[] split = crazyString.split("");
-			
-			System.out.println("4");
-			
-			Integer length = crazyString.length();
-
-			System.out.println("5");
-			
-			// Start rendering text randomly
-			Random rand = new Random();
-			
-			System.out.println("6");
-			
-			int i = 0;
-			int j = 0;
-			
-			System.out.println("7");
-			
-			while (true) {
-				System.out.print("While; i: ");
-				System.out.println(i);
-				
-				Integer nextChar = rand.nextInt(length);
-				String c = split[nextChar];
-				runOnUiThread(new Runnable() {
-				     public void run() {
-				    	Intent intent = getIntent();
-				    	String crazyString = intent.getStringExtra(MainActivity.CRAZY_STRING);
-				    	String[] split = crazyString.split("");
-				    	// Get the text view
-						TextView textView = (TextView) findViewById(R.id.crazy_name);
-//				    	textView.append(split[nextChar]);
-				    }
-				});
-				
-				
-				System.out.print("Appended: ");
-				System.out.println(split[nextChar]);
-				
-				if (i >= 99) {
-					i = 0;
-					j++;
-					if (j >= 50) {
-						break;
+	// Handler functions
+	public void startHandler() {
+		super.onStart();
+		
+		Thread background=new Thread(new Runnable() {
+			public void run() {
+				try {
+					// Get context 
+					Intent intent = getIntent();
+					String crazyString = intent.getStringExtra(MainActivity.CRAZY_STRING);
+					
+					// Set stage for looping
+					String[] split = crazyString.split("");
+					Integer length = crazyString.length();
+					
+					Random rand = new Random();
+					for(int i=0; i < 500; i++){
+						Integer nextChar = rand.nextInt(length);
+						String c = split[nextChar];
+						Message msg = handler.obtainMessage();
+						msg.obj = c;
+						handler.sendMessage(msg); // want to send string c to be appended to text view
+						Thread.sleep(50);
 					}
 				}
-				i++;
+				catch (Throwable t) {
+					// just end the background thread
+				}
 			}
-		}
-	};
-
+		});
+		
+		isRunning.set(true);
+		background.start();
+	}
+	
+	public void onStop() {
+		super.onStop();
+		isRunning.set(false);
+	}
 }
